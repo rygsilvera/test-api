dfc3c610007614a6303ef809bcaed117ca8b14a2
Ryan Silvera <rygsilvera@gmail.com>
2024-10-14 13:07:21 -0400

    Added index.js, placeholder ignore files, and dockerfile used to test creating Docker containers locally

--- api.js
+++ api.js
@@ -1,19 +1,26 @@
 // api.js
 
 import express from "express";
-import {getCollection} from "../test-database/database.objects.js";
+import {getCollection} from "../test-database/database.objects.js"; // Comment out when testing Docker container creation
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+
+/* 
+# For Docker Testing
+app.get("/", (req,res) => {
+  res.json({ message: `Docker is working! =)` });
+});
+*/
 
 app.get("/collection", async (req,res) => {
   const collection = await getCollection();
   res.send(collection);
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
-})
+});
 
 /*
 # Functions to Add
